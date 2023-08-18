879b441ae04d8a6fa3501b72de93555da6c0d89d
svet-khol <svetlana.kholodnyak@gmail.com>
2023-08-18 20:09:06 +0300

    new formula

--- js/formula.js
+++ js/formula.js
@@ -1,33 +1,37 @@
 let
     start = document.querySelector('.calc'),
     result = document.querySelector('.result'),
-    heart_rate /* ЧСС - частота сердечных сокращений, мин-1; */ ,
+    Z,
+    age
+    /* возраст дебюта СНнФВ - возраст пациента, в котором дебютировала клиника
+    СНнФВ, лет */
+    ,
+    /*heart_rate  ЧСС - частота сердечных сокращений, мин-1; ,*/
+
     respiratory_rate /* ЧДД - частота дыхательных движений, мин - 1; */ ,
     systolic_pressure
     /* сист.АД - уровень систолического артериального давления, 
                       измеренный на 3 - 5 минуте ортостаза, мм рт.ст.;*/
     ,
-    concentration_N_peptide_blood_serum
-/*   NT-proBNP - концентрация N - концевого предшественника мозгового
-натрийуретического пептида в сыворотке крови, пг / мл, */
-;
+    LYM /*1 уровень лимфоцитов, %*/ ,
+    RDW /* ширина распределения эритроцитов по объему, % */ ;
+
+
 start.onclick = function (event) {
     event.preventDefault();
-    heart_rate = document.getElementById('heart_rate').value;
+    age = document.getElementById('age').value;
     respiratory_rate = document.getElementById('respiratory_rate').value;
     systolic_pressure = document.getElementById('systolic_pressure').value;
-    concentration_N_peptide_blood_serum = document.getElementById('concentration_N_peptide_blood_serum').value;
-    aa4 = document.getElementById('aa4').value;
+    LYM = document.getElementById('LYM').value;
+    RDW = document.getElementById('RDW').value;
 
-    S_t = Math.exp(-0.514056 *
-        Math.exp(
-            (0.019 * heart_rate) +
-            (0.169 * aa4) +
-            (-0.049 * systolic_pressure) +
-            (0.000003 * concentration_N_peptide_blood_serum)
-        )
-    ) * 100;
-    result.innerHTML = result.innerHTML + S_t + '%';
+
+    Z = (-0.075 * age) +
+        (0.290 * respiratory_rate) +
+        (-0.04 * systolic_pressure) +
+        (-0.051 * LYM) +
+        (0.161 * RDW);
+    result.innerHTML = result.innerHTML + Z;
 }
 
 
